aecdbb832df08689da394b85be9e353b4bf3b7db
Веня Vasilyuk <dashka.chernichka14@gmail.com>
2022-07-26 16:47:41 +0400

    create component 'Popup'

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Route, Switch } from 'react-router';
 import { Home } from './pages/Home/Home';
 import { MonchStatistics } from './pages/MonchStaticticks/MonchStatistics';
 import { Header} from './shared/Header/Header';
+import { Popup} from './shared/Popup/Popup';
 
 function App() {
   return ( 
-    <div className="container">
-      <Header/>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/month-statistics' exact component={MonchStatistics} />
-      </Switch>
+    <div className='global-container'>
+      <Popup />
+      <div className="container">
+        
+        <Header/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/month-statistics' exact component={MonchStatistics} />
+        </Switch>
+      </div>
     </div>
   );
 }
